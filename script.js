1c39c2bc3aa50111f3f9855196ece17c34e2be35
Emmanuel Kabange <emmanuelwelek@gmail.com>
2024-07-28 14:30:30 +0000

    javascript code added

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const countBtn = document.getElementById("countBtn");
+const countNumber = document.getElementById("count");
 
+let count = 0;
+
+function updateCount() {
+    count++;
+    countNumber.textContent = count;
+}
+
+countBtn.addEventListener("click", updateCount);
